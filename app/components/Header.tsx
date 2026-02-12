e9adc9e553a64f926e651cc02a09d29df9a017bd
olgatenison <88651744+olgatenison@users.noreply.github.com>
2026-02-12 14:20:39 +0200

    logo

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -40,7 +40,7 @@ export default function Header() {
                 alt="iProk"
                 width={300}
                 height={32}
-                src="/img/logo.jpg"
+                src="/img/logo.webp"
                 className="h-8 w-auto"
                 priority
               />
